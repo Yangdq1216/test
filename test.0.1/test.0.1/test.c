77f3728870c1ce6ee82d9408721778c466e99c0f
ydq1223636496 <1223636496@qq.com>
2023-11-19 09:33:51 +0800

    第一次

---
+++ test.0.1/test.0.1/test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+//int main()
+//{
+//
+//	int sb = 20;
+//	double price = 66.6;
+//
+//
+//		return 0;
+//
+//}
+//全局变量- 定义在代码块（{}）之外的变量
+//int main()
+//{
+//	int a= 10;//局部变量定义在{}内的变量
+//	printf("%d\n", a);
+//
+//
+//	return 0;
+//
+//int main()
+//{
+//	//计算两个数的和
+//	int num1 = 2;
+//	int num2 = 3;
+//	int sum = 5;
+//	//输入数据-使用输入函数scanf
+//	scanf_s("%d%d", &num1, &num2);//&取地址符号
+//
+//	//c语言语法规定，变量要定义在当前代码块的最前面
+//	sum=num1 + num2;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int num = 0;
+//
+//	printf("num=%d\n", num);
+//
+//
+//
+//
+//
+//	return 0;
+//}
+int main()
+{
+	printf("hello world\n");
+	return 0;
+}
